cf8506669200c6d0039a81652946d5764fd8ed39
surendra-chouhan <chouhansurendra028@gmail.com>
2021-02-26 11:55:44 +0530

    Added Code for choosing option between X|O

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,9 +1,12 @@
 
 import java.util.*;
+import java.util.Scanner;
 
 public class TicTacToe {
-	static char[] board;
-
+	public static char[] board;
+	public static String PlayerLetter;
+	public static String ComputerLetter;
+	
 	static char[] createBoard() {
 
 		char[] board = new char[10];
@@ -14,9 +17,35 @@ public class TicTacToe {
 		return board;
 	}
 
+	public static String choose(String option) {
+		
+		if(option.equals("X")) { 
+			PlayerLetter = "X";
+			ComputerLetter = "O";
+		}
+		else if(option.equals("O")) {
+			ComputerLetter = "X";
+			PlayerLetter = "O";
+		}
+		else {
+			System.out.println("Incorrect Input");
+		}
+		return ComputerLetter;
+	}
 	public static void main(String[] args) {
 		System.out.println("Welocme to Tic-Tac-Toe program");
 		
-		createBoard();
+		
+		TicTacToe tictac = new TicTacToe();
+		board = new char[10];//createBoard();
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Please Choose X or O");
+		String option = sc.next();		
+		
+		tictac.choose(option);
+		System.out.println("Player is : " + PlayerLetter);
+		System.out.println("Computer is : " + ComputerLetter);
 	}
 }
