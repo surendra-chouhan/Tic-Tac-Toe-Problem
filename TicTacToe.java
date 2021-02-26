735ccde12462b85c66c0f6b1467335e09cbd68d4
surendra-chouhan <chouhansurendra028@gmail.com>
2021-02-26 10:05:00 +0530

    Added code to create a board

--- TicTacToe.java
+++ TicTacToe.java
@@ -2,7 +2,21 @@
 import java.util.*;
 
 public class TicTacToe {
+	static char[] board;
+
+	static char[] createBoard() {
+
+		char[] board = new char[10];
+		
+		for(int i = 1; i < 10; i++) {
+			board[i] = ' ';
+		}
+		return board;
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welocme to Tic-Tac-Toe program");
+		
+		createBoard();
 	}
 }
