8735cd8a2b5869884a9cf56245a0aa4ecd9f8795
surendra-chouhan <chouhansurendra028@gmail.com>
2021-02-28 16:47:40 +0530

    Added code to check for free space and then make Computer move

--- TicTacToe.java
+++ TicTacToe.java
@@ -5,6 +5,7 @@ public class TicTacToe {
 	public static char[] board;
 	public static String PlayerLetter;
 	public static String ComputerLetter;
+	static Scanner sc = new Scanner(System.in);
 	
 	static char[] createBoard() {
 
@@ -40,14 +41,12 @@ public class TicTacToe {
 		System.out.println (board[7] + " | " + board[8] + " | " + board[9]);
 	}
 	
-	public static void makeMove(){
+	public static void makePlayerMove(){
 
-		Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the position you want to move to : "
+		System.out.println("Enter the position you want to move to : "
         					+ "\nPosition must be betwween 1 to 9");
         int position = sc.nextInt();
 
-        //for(int i = 1; i < 10; i++){
         if(position >= 1 && position <= 9)
         	board[position] = PlayerLetter.charAt(0);
         else
@@ -55,16 +54,24 @@ public class TicTacToe {
         
         showBoard();
 	}
+	
+	public static void makeComputerMove() {
+		int cposition = (int)Math.floor(Math.random() * (10-1))+1;
+		
+		if(board[cposition] == 0 ) {
+			System.out.println("Computer will put " + ComputerLetter.charAt(0) + " at position : " + cposition);
+			board[cposition] = ComputerLetter.charAt(0);
+		}
+		
+		showBoard();
+	}
 
 	
 	public static void main(String[] args) {
 		System.out.println("Welocme to Tic-Tac-Toe program");
 		
 		
-		//TicTacToe tictac = new TicTacToe();
-		board = new char[10];//createBoard();
-		
-		Scanner sc = new Scanner(System.in);
+		board = new char[10];
 		
 		System.out.println("Please Choose X or O");
 		String option = sc.next().toUpperCase();		
@@ -74,6 +81,7 @@ public class TicTacToe {
 		System.out.println("Computer is : " + ComputerLetter);
 	
 		showBoard();
-        makeMove();
+        makePlayerMove();
+        makeComputerMove();
 	}
 }
